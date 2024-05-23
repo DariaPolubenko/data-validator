9b4a30cda88e0a6106dbf0ae98d386394f7642db
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-23 22:00:54 +0700

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,4 +26,8 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.jacocoTestReport { reports { xml.required.set(true) } }
+tasks.jacocoTestReport {
+    reports {
+        xml.required = true
+    }
+}
