c8c0fe420962b4210ffe352cb44ec1f0a7932544
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-24 05:57:00 +0700

    update Validator.java

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -1,2 +1,16 @@
-package schemas;public class Validator {
+package hexlet.code;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import schemas.StringSchema;
+
+@NoArgsConstructor
+public class Validator {
+    //private StringSchema schema;
+
+    public StringSchema string() {
+        var schema = new StringSchema();
+        return schema;
+    }
+
 }
