fd31d9473294e224dfab021ab8b4899eaefa9eac
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-24 05:54:50 +0700

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,14 @@
 package hexlet.code;
 
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+
+        var v = new Validator();
+        var schema = v.string().minLength(3);
+                //.required();
+                //minLength(3).contains("wh");
+        System.out.println(schema.isValid("lala"));
+
     }
 }
