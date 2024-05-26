f5c428e9240c23c910de5cb900b15305a2654457
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-27 05:52:04 +0700

    update Schema.java

--- app/src/main/java/schemas/Schema.java
+++ app/src/main/java/schemas/Schema.java
@@ -1,2 +1,21 @@
-package schemas;public class Schema {
+package schemas;
+
+public abstract class Schema {
+
+    public boolean isRequired() throws Exception {
+        var value = (boolean) getValue("notNull");
+        return value;
+    }
+
+    public Object getValue(String fieldName) throws Exception {
+        var field = this.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        var value = field.get(this);
+        field.setAccessible(false);
+
+        return value;
+    }
+
+    public abstract boolean isValid(Object object) throws Exception;
+
 }
