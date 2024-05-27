a6819a84695388ce8e0fd52b103a659790e24ff2
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-27 22:51:33 +0700

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,8 @@
 package hexlet.code;
 
 
+import java.util.HashMap;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
@@ -8,5 +10,14 @@ public class App {
         var schema = v.number();
 
         System.out.println(schema.isValid(5)); //true
+
+
+        var v1 = new Validator();
+        var schema1 = v1.map();
+
+        var data = new HashMap<String, String>();
+        data.put("key1", "value1");
+
+        schema1.isValid(data);
     }
 }
