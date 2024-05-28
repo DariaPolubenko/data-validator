c3b22eeddda76f3ef4cf4108241457c5b0ca1167
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-29 04:22:51 +0700

    update all StringSchema.java

--- app/src/main/java/schemas/StringSchema.java
+++ app/src/main/java/schemas/StringSchema.java
@@ -1,66 +1,16 @@
 package schemas;
 
-public class StringSchema extends BaseSchema {
-    private boolean notNull;
-    private Integer minLength;
-    private String symbols;
 
-    public StringSchema required() {
-        this.notNull = true;
-        return this;
-    }
+public class StringSchema extends BaseSchema<String> {
 
-    public StringSchema minLength(Integer count) throws Exception {
-        if (count < 0) {
-            throw new Exception("Длина не может быть меньше 0");
-        } else {
-            this.minLength = count;
-        }
+    public StringSchema minLength(int length) {
+        addCheck("minLength", value -> value.length() >= length);
         return this;
     }
 
     public StringSchema contains(String characters) {
-        this.symbols = characters;
+        addCheck("contains", value -> value.contains(characters));
         return this;
-    }
-
-    public boolean isValid(Object object) throws Exception {
-        var text = (String) object;
-
-        if (isRequired()) {
-            if (text == null || text.equals("")) {
-                return false;
-            }
-        }
-        if (hasMinLength()) {
-            if (text == null || text.length() < this.minLength) {
-                return false;
-            }
-        }
-        if (hasContains()) {
-            if (!text.contains(this.symbols)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public boolean hasMinLength() throws Exception {
-        var value = (Integer) getValue("minLength");
-        if (value == null) {
-            return false;
-        } else if (value < 0) {
-            throw new Exception("Длина не может быть отрицательная");
-        }
-        return true;
-    }
-
-    public boolean hasContains() throws Exception {
-        var value = getValue("symbols");
-        if (value == null) {
-            return false;
-        }
-        return true;
     }
 
 }
