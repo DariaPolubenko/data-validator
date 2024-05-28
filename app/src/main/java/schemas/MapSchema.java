66adf0ba1b7a1ab693a41a1a507d44b21ae3a684
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-29 04:22:18 +0700

    update all MapSchema.java

--- app/src/main/java/schemas/MapSchema.java
+++ app/src/main/java/schemas/MapSchema.java
@@ -1,54 +1,30 @@
 package schemas;
 
 import java.util.Map;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.function.Predicate;
 
+public class MapSchema extends BaseSchema<Map<String, String>> {
 
-public class MapSchema extends BaseSchema {
-    private boolean notNull;
-    private Integer minSize;
-
-    public MapSchema required() {
-        this.notNull = true;
-        return this;
-    }
-
-    public MapSchema sizeof(Integer count) throws Exception {
-        if (count < 0) {
-            throw new Exception("Длина не может быть меньше 0");
-        } else {
-            this.minSize = count;
-        }
-        return this;
-    }
-
-    public boolean isValid(Object object) throws Exception {
-        ObjectMapper oMapper = new ObjectMapper();
-        var data = oMapper.convertValue(object, Map.class);
-
-        if (isRequired()) {
-            if (data == null) {
-                return false;
-            }
-        }
-        if (hasMinSize()) {
-            if (!data.isEmpty()) {
-                if (data.size() < minSize) {
+    public MapSchema sizeof(Integer minSize) {
+        Predicate<Map<String, String>> fn = value -> {
+            if (!value.isEmpty()) {
+                if (value.size() < minSize) {
                     return false;
                 }
             }
-        }
-        return true;
+            return true;
+        };
+        addCheck("sizeof", fn);
+        return this;
     }
 
-    public boolean hasMinSize() throws Exception {
-        var value = (Integer) getValue("minSize");
-        if (value == null) {
-            return false;
-        }
-        return true;
+    public MapSchema minLength(int length) {
+        return this;
     }
 
-
+    public MapSchema contains(String characters) {
+        return this;
+    }
 
 }
+
