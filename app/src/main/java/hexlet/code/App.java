e014a6988d2c791cc28123c96737bf21d98e651c
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-30 02:38:50 +0700

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,31 +1,7 @@
 package hexlet.code;
 
-
-import schemas.BaseSchema;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-
-        var v = new Validator();
-        var schema = v.map();
-
-        Map<String, BaseSchema<String>> schemas = new HashMap<>();
-        schemas.put("firstName", v.string().required());
-        schemas.put("lastName", v.string().required().minLength(2));
-
-        schema.shape(schemas);
-
-        Map<String, String> human2 = new HashMap<>();
-        human2.put("firstName", "John");
-        human2.put("lastName", null);
-
-        System.out.println(schema.isValid(human2));
-
-        System.out.println(v.string().required().isValid("John"));
-        System.out.println(v.string().required().minLength(2).isValid(null));
     }
 }
