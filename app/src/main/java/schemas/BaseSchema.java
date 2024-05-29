9b74da1c03cb0ef6a8a761bdd5adaf59d5c770c9
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-29 23:00:14 +0700

    update BaseSchema.java

--- app/src/main/java/schemas/BaseSchema.java
+++ app/src/main/java/schemas/BaseSchema.java
@@ -8,16 +8,19 @@ import java.util.function.Predicate;
 public class BaseSchema<T>  {
     protected Map<String, Predicate<T>> checks = new LinkedHashMap<>();
     protected boolean isNotNull;
+    protected T data;
 
     protected void addCheck(String name, Predicate<T> validate) {
         checks.put(name, validate);
     }
 
-    public boolean isValid(T data) {
-        if (data == null & isNotNull) {
+    public boolean isValid(T object) {
+        if (object == null & isNotNull) {
             return false;
         }
+        data = object;
+
         return checks.values().stream()
-                    .allMatch(fn -> fn.test(data));
+                    .allMatch(fn -> fn.test(object));
     }
 }
