d9857d751e4886dc25c06bdeba826d91bb5da4af
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-29 20:34:28 +0700

    update MapSchema.java

--- app/src/main/java/schemas/MapSchema.java
+++ app/src/main/java/schemas/MapSchema.java
@@ -5,25 +5,28 @@ import java.util.function.Predicate;
 
 public class MapSchema extends BaseSchema<Map<String, String>> {
 
+    public MapSchema required() {
+        Predicate<Map<String, String>> fn = value -> {
+            if (value == null) {
+                return false;
+            }
+            return true;
+        };
+        addCheck("required", fn);
+        return this;
+    }
+
     public MapSchema sizeof(Integer minSize) {
         Predicate<Map<String, String>> fn = value -> {
             if (!value.isEmpty()) {
-                if (value.size() < minSize) {
+                if (value.size() != minSize) {
                     return false;
                 }
             }
             return true;
         };
         addCheck("sizeof", fn);
-        return this;
-    }
-
-    public MapSchema minLength(int length) {
-        return this;
-    }
-
-    public MapSchema contains(String characters) {
+        isNotNull = true;
         return this;
     }
 }
-
