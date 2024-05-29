592c83b37ea2e1e33d872d1bfab78a86cde65f51
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-29 20:34:57 +0700

    update v

--- app/src/main/java/schemas/StringSchema.java
+++ app/src/main/java/schemas/StringSchema.java
@@ -1,16 +1,30 @@
 package schemas;
 
 
+import java.util.function.Predicate;
+
 public class StringSchema extends BaseSchema<String> {
+
+    public StringSchema required() {
+        Predicate<String> fn = value -> {
+            if (value == null || value.equals("")) {
+                return false;
+            }
+            return true;
+        };
+        addCheck("required", fn);
+        return this;
+    }
 
     public StringSchema minLength(int length) {
         addCheck("minLength", value -> value.length() >= length);
+        isNotNull = true;
         return this;
     }
 
     public StringSchema contains(String characters) {
         addCheck("contains", value -> value.contains(characters));
+        isNotNull = true;
         return this;
     }
-
 }
