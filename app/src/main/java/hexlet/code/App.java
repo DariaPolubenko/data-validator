7c341643ab18eb37ed0377019d2cd47d0df56a43
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-29 20:33:58 +0700

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,32 +1,36 @@
 package hexlet.code;
 
+
 public class App {
     public static void main(String[] args) throws Exception {
 
         var v = new Validator();
         var schema = v.string();
 
-        schema.required();
-        //schema.minLength(2);
+        //schema.required();
+        schema.minLength(2);
         //schema.contains("lo");
 
-        System.out.println(schema.isValid("")); //false
-        System.out.println(schema.isValid(null)); //false
-
-        /*
-        var v = new Validator();
-        var schema = v.number();
-
-        System.out.println(schema.isValid(5)); //true
+        //System.out.println(schema.isValid("")); //false
+        //System.out.println(schema.isValid(null)); //false
 
 
         var v1 = new Validator();
-        var schema1 = v1.map();
+        var schema1 = v1.number();
+
+        System.out.println(schema1.isValid(5)); //true
+
+        //schema1.positive();
+        schema1.range(1, 3);
+        System.out.println(schema1.isValid(null)); //false
+/*
+        var v2 = new Validator();
+        var schema2 = v2.map();
 
         var data = new HashMap<String, String>();
         data.put("key1", "value1");
 
-        schema1.isValid(data);
+        schema2.isValid(data);
 
          */
     }
