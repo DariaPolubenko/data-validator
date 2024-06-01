b85636a851208ce7a7bcfa1022175c1ab87f78f0
Daria Polubenko <daria.polubenko@gmail.com>
2024-06-02 06:33:11 +0700

    update StringSchema.java

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -3,7 +3,7 @@ package hexlet.code.schemas;
 
 import java.util.function.Predicate;
 
-public class StringSchema extends BaseSchema<String> {
+public final class StringSchema extends BaseSchema<String> {
 
     public StringSchema required() {
         Predicate<String> fn = value -> {
