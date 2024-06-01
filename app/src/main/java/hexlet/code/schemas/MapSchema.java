8b8d0b1e85a70a25e0941a1e82faa5f95be770b6
Daria Polubenko <daria.polubenko@gmail.com>
2024-06-02 05:09:36 +0700

    update MapSchema.java

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -3,37 +3,38 @@ package hexlet.code.schemas;
 import java.util.Map;
 import java.util.function.Predicate;
 
-public final class MapSchema extends BaseSchema<Map<?, String>> {
+public final class MapSchema extends BaseSchema<Map<?, ?>> {
 
     public MapSchema required() {
-        Predicate<Map<?, String>> fn = value -> {
+        addCheck("required", value -> {
             if (value == null) {
                 return false;
             }
             return true;
-        };
-        addCheck("required", fn);
+        });
         return this;
     }
 
     public MapSchema sizeof(Integer minSize) {
-        Predicate<Map<?, String>> fn = value -> {
+        addCheck("sizeof", value -> {
             if (!value.isEmpty()) {
                 if (value.size() != minSize) {
                     return false;
                 }
             }
             return true;
-        };
-        addCheck("sizeof", fn);
+        });
         isNotNull = true;
         return this;
     }
 
     public MapSchema shape(Map<?, BaseSchema<String>> object) {
-        Predicate<Map<?, String>> fn = value ->
-                object.keySet().stream().allMatch(key -> object.get(key).isValid(value.get(key)));
-        addCheck("shape", fn);
+        addCheck("shape", value ->
+                object.keySet().stream().allMatch(key -> {
+                    var schema = object.get(key);
+                    var strValue = (String) value.get(key);
+                    return schema.isValid(strValue);
+                }));
         return this;
     }
 }
