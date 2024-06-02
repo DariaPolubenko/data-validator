34d6a14049555c7f1f7eb3bc220da57355f9f391
Daria Polubenko <daria.polubenko@gmail.com>
2024-06-02 07:09:19 +0700

    update BaseSchema.java

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -7,11 +7,11 @@ import java.util.function.Predicate;
 public abstract class BaseSchema<T>  {
     protected Map<String, Predicate<T>> checks = new LinkedHashMap<>();
 
-    protected void addCheck(String name, Predicate<T> validate) {
+    protected final void addCheck(String name, Predicate<T> validate) {
         checks.put(name, validate);
     }
 
-    public boolean isValid(T value) {
+    public final boolean isValid(T value) {
         return checks.values().stream()
                     .allMatch(fn -> fn.test(value));
     }
